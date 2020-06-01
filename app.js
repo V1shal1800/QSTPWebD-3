45ddd1b4aaeff8ca7e7c4534b61b4b1fe19377ee
V1shal1800 <63504643+V1shal1800@users.noreply.github.com>
2020-06-01 22:24:52 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+const form ={};
+
+form.text = document.querySelector('#input_text');
+form.translateButton = document.querySelector('#TButton');
+
+const TranslatedText = document.querySelector('#TranslatedText');
+const TargetArea = document.querySelector('#TargetArea')
+
+function Translate(){
+    let text =form.text.value;
+    TargetArea.innerHTML = "";
+
+    fetch("https://api.funtranslations.com/translate/pirate.json", {
+        body: `text=${text}`,
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        method: "POST"
+    })
+    .then(response => {
+        return response.json();
+    }).then(data =>{
+        TargetArea.innerHTML = data.contents.translated;
+        TranslatedText.style = "display:block";
+    })
+    .catch(err => {
+        console.log(err);
+        alert("Request Limit Exceeded Or Server Side Error!!")
+    });
+
+    form.text.value = "";
+}
+
+
+form.translateButton.addEventListener('click', (e)=>{
+    e.preventDefault();
+    if (form.text.value != '') {
+        Translate();
+      }
+    else{
+        alert('Field cannot be empty');
+      }
+})
